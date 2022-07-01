b76d13e5e1d49bed4d633f2de05fe67c7e3841b6
Taylor Dolan <taylordolan@me.com>
2022-06-30 22:10:01 -0400

    add example plan

--- src/stores.js
+++ src/stores.js
@@ -26,8 +26,44 @@ const _newPlan = () => {
   return plan;
 }
 
-const _plans = () => {
-  return [_newPlan()];
+const _example = () => {
+  return [
+    {
+      label: "Example Plan",
+      notes: "",
+      showNotes: false,
+      turns: [
+        {
+          description: "current values",
+          resources: { c: 2, o: 0, k: 4, q: 0, v: 0 },
+          excluded: false,
+          completed: false,
+          id: 0,
+        },
+        {
+          description: "research artificial intelligence",
+          resources: { c: 0, o: 0, k: -4, q: 1, v: 0 },
+          excluded: false,
+          completed: false,
+          id: 1,
+        },
+        {
+          description: "(free action)",
+          resources: { c: 0, o: 1, k: 0, q: 0, v: 0 },
+          excluded: false,
+          completed: false,
+          id: 2,
+        },
+        {
+          description: "mine gaia planet",
+          resources: { c: -2, o: -1, k: 0, q: -1, v: 0 },
+          excluded: false,
+          completed: false,
+          id: 3,
+        },
+      ],
+    }
+];
 }
 
 export const newTurn = readable(_newTurn);
@@ -36,7 +72,7 @@ export const newPlan = readable(_newPlan);
 const itemName = "plans";
 const retrieved = localStorage.getItem(itemName);
 const parsed = JSON.parse(retrieved);
-export const plans = writable(parsed === null ? _plans() : parsed);
+export const plans = writable(parsed === null ? _example() : parsed);
 
 plans.subscribe(value =>
   localStorage.setItem(itemName, JSON.stringify(value))
